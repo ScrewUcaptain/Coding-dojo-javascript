2a7294db9289c3efc633b36e1894ee293b1ad486
ScrewUcaptain <basilebrusseau@gmail.com>
2022-12-27 14:17:37 +0100

    step2 arraySum done

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 module.exports = {
     arraySum: function(arr) {
         // Your code goes here
-    
-        return arr; //or something else who knows ?
-    },
+        if (arguments.length === 0) return 0
 
+        const charOrNum = element => isNaN(element) ? 0 : +element 
+
+        return arr.reduce((prev, current) => charOrNum(prev) + charOrNum(current))
+         //or something else who knows ?
+    },
+    
     arrayAverage: function(arr) {
         // Your code goes here
-
+        if (arguments.length === 0) return 0
+        
         return arr; // or something else who knows ?
     },
-
+    
     arrayMax: function(arr) {
         // Your code goes here
-
+        if (arguments.length === 0) return 0
+        
         return arr // or something else who knows ?
     },
-
+    
     arrayMin: function(arr) {
         // Your code goes here
+        if (arguments.length === 0) return 0
 
         return arr // or something else who knows ?
     }
