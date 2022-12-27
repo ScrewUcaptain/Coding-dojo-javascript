600b703f6cb4450665ab67d1822deb16e6fc1cd9
ScrewUcaptain <basilebrusseau@gmail.com>
2022-12-27 12:20:12 +0100

    complete step3

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 module.exports = {
     falsyBouncer: function(arr) {
         // your code goes here
+        if(arguments.length === 0) return []
 
+        return arr.filter(Boolean)
         return arr // or something else who knows ? 
     }
 }
