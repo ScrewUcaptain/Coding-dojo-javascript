399b16020fff2e95f9e00cf3c5c78fe1468efa62
ScrewUcaptain <basilebrusseau@gmail.com>
2022-12-27 13:14:42 +0100

    step4 done

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
  module.exports = {
     palindromeChecker: function(s) {
         // Your code goes here
-    
-        return s //something
+        const stringReverser = str => str.split('').reverse().join('')
+        
+        const regex = /[^a-zA-Z]/g
+        s = s.replace(regex, "").toLowerCase()
+        let str = stringReverser(s)
+
+
+
+
+        return s === str //something
     }
  }
